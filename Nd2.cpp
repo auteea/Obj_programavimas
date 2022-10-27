1a157698ef192d31c2db1fcf6e96a3ecb2b3482a
auteea <113633037+auteea@users.noreply.github.com>
2022-10-27 12:19:34 +0300

    Add files via upload

---
+++ Nd2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <stdlib.h>
+#include <sstream>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <random>
+#include <numeric>    
+#include "Studentas2.h"
+#include "Funkcijos2.h"
+
+
+int main()
+{
+    string a;
+    vector<studentas> grupe;
+    cout << "Jei norite duomenis ivesti pats, spauskite 'x', jei nuskaityti is failo 'y'\n";
+    cin >> a;
+    while (a != "x" && a!= "y") {
+        cout << "Klaida, iveskite x arba y ";
+        cin.clear();
+        cin.ignore(256, '\n');
+        cin >> a;
+    }
+    if (a == "x") {
+        grupe = ivedimas();
+        print_student(grupe, 0);
+    }
+    else {
+        int pazymiu_sk;
+        char temp;
+        std::vector<studentas> Eil;
+        read_from_file(Eil, &pazymiu_sk);
+        print_student(Eil, pazymiu_sk);
+        system("pause");
+        return 0;
+    }
+
+}
+
---
+++ Studentas2.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+struct studentas {
+    string Vard, Pav, ivestis = " ";
+    vector<int> paz;
+    int egz;
+    float GP, med = 0.0;
+};
